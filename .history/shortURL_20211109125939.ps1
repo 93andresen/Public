470b4161705d4fd2e63d2652ec90f1c6349c2d20
Richard Andresen <93andresen@gmail.com>
2021-11-09 13:00:28 +0100

    rr

--- .history/shortURL_20211109125939.ps1
+++ .history/shortURL_20211109125939.ps1
@@ -8,10 +8,12 @@ if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
 else{
     # Installing winget from the Microsoft Store
 	Write-Host "Winget not found, installing it now."
+    $ResultText.text = "`r`n" +"`r`n" + "Installing Winget... Please Wait"
 	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
 	$nid = (Get-Process AppInstaller).Id
 	Wait-Process -Id $nid
 	Write-Host Winget Installed
+    $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
 }
 
 Write-Host "Installing Autohotkey"
