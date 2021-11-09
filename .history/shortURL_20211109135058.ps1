43325ae703ac664616fc80f2a203bb13045950e7
Richard Andresen <93andresen@gmail.com>
2021-11-09 13:53:56 +0100

    --- Auto-Git Commit ---

---
+++ .history/shortURL_20211109135058.ps1
@@ -0,0 +1,39 @@
+# GUI Specs
+Write-Host "Checking winget..."
+
+# Check if winget is installed
+if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
+    'Winget Already Installed'
+}  
+else{
+    # Installing winget from the Microsoft Store
+	Write-Host "Winget not found, installing it now."
+	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+	$nid = (Get-Process AppInstaller).Id
+	Wait-Process -Id $nid
+	Write-Host Winget Installed
+}
+
+
+
+if (Test-Path "C:\Program Files\AutoHotkey\AutoHotkey.exe"){
+    'Autohotkey Already Installed'
+}  
+else{
+    Write-Host "Installing Autohotkey"
+    winget install -e Lexikos.AutoHotkey | Out-Host
+    Write-Host "Installed Autohotkey"
+}
+
+mkdir C:\zzz_temp
+cd C:\zzz_temp
+# Source file location
+$source = 'https://raw.githubusercontent.com/93andresen/Richard_Public/main/ahk_script_test_delete.ahk'
+# Destination to save the file
+$destination = 'c:\zzz_temp\ahk_script_test_delete.ahk'
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
+
+& "C:\Program Files\AutoHotkey\AutoHotkey.exe" c:\zzz_temp\ahk_script_test_delete.ahk
+
+
