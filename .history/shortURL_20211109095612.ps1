80dfd194965cd484c7023319a72a3b5d051661ef
Richard Andresen <93andresen@gmail.com>
2021-11-09 10:48:58 +0100

    JJ

---
+++ .history/shortURL_20211109095612.ps1
@@ -0,0 +1,40 @@
+if (Test-Path "C:\Program Files\AutoHotkey\AutoHotkey.exe"){
+    'Autohotkey Already Installed'
+}  
+else{
+    Write-Host "Checking winget..."
+    # Check if winget is installed
+    if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
+        'Winget Already Installed'
+    }  
+    else{
+        # Installing winget from the Microsoft Store
+        Write-Host "Winget not found, installing it now."
+        Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+        $nid = (Get-Process AppInstaller).Id
+        Wait-Process -Id $nid
+        Write-Host Winget Installed
+    }
+    Write-Host "Installing Autohotkey"
+    winget install -e Lexikos.AutoHotkey | Out-Host
+    Write-Host "Installed Autohotkey"
+}
+
+'Downloading ahk script to C:\zzz_temp'
+if (Test-Path "C:\zzz_temp"){
+}  
+else{
+    mkdir C:\zzz_temp
+}
+Set-Location C:\zzz_temp
+# Source file location
+$source = 'https://raw.githubusercontent.com/93andresen/Richard_Public/main/RunAnywhere.ahk'
+# Destination to save the file
+$destination = 'RunAnywhere.ahk'
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
+& "C:\Program Files\AutoHotkey\AutoHotkey.exe" c:\zzz_temp\RunAnywhere.ahk
+'running c:\zzz_temp\RunAnywhere.ahk'
+
+#Set-Location C:\
+#Remove-Item C:\zzz_temp -Recurse
