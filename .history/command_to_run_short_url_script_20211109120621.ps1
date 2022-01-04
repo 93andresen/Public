82541033a12fb9963b92f21e54a3b05bb68d4b99
93andresen@gmail.com <93andresen@gmail.com>
2022-01-04 12:59:03 +0100

    --- Auto-Git Commit ---

--- .history/command_to_run_short_url_script_20211109120621.ps1
+++
@@ -1,5 +0,0 @@
-#iwr https://git.io/JXaaD|iex
-"Running O&O Shutup with Recommended Settings"
-#iwr git.io/JXaaD|iex
-
-#iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JXaaD'))
