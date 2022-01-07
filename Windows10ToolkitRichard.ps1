4eafcf6c4ec232d888070c54756108656bde9a60
93andresen@gmail.com <93andresen@gmail.com>
2022-01-07 04:25:15 -0800

    --- Auto-Git Commit ---

--- Windows10ToolkitRichard.ps1
+++ Windows10ToolkitRichard.ps1
@@ -33,6 +33,7 @@ else{
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 choco feature enable -n=allowGlobalConfirmation -y
+choco feature enable -n allowEmptyChecksums -y
 
 if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
     'Winget Already Installed'
