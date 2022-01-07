e36a6d394706589ac254e06aab492a54e7fa6218
93andresen@gmail.com <93andresen@gmail.com>
2022-01-07 09:22:48 -0800

    yuio

--- Windows10ToolkitRichard.ps1
+++ Windows10ToolkitRichard.ps1
@@ -2,13 +2,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 'Downloaded files will be saved in C:\temp_Windows10ToolkitRichard'
 if (Test-Path "C:\temp_Windows10ToolkitRichard"){
-    Remove-Item "C:\temp_Windows10ToolkitRichard"
-}  
-else{
-    mkdir C:\temp_Windows10ToolkitRichard
+    Remove-Item -LiteralPath "C:\temp_Windows10ToolkitRichard" -Force -Recurse
 }
+Start-Sleep -5000
+mkdir C:\temp_Windows10ToolkitRichard
 Set-Location C:\temp_Windows10ToolkitRichard
-
+Start-Sleep -5000
 Write-Host "Creating Ststem Restore Point and naming it: Before_Running_Windows10ToolkitRichard"
 Enable-ComputerRestore -Drive "C:\"
 Checkpoint-Computer -Description "Before_Running_Windows10ToolkitRichard" -RestorePointType "MODIFY_SETTINGS"
@@ -27,11 +26,8 @@ if (Test-Path "C:\ProgramData\chocolatey\bin\choco.exe"){
 }  
 else{
     'Installing Chocolatey'
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
-choco feature enable -n=allowGlobalConfirmation -y
-choco feature enable -n allowEmptyChecksums -y
-
 if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
     'Winget Already Installed'
 }  
@@ -63,10 +59,10 @@ else{
         }
     }
 }
-
+choco feature enable -n=allowGlobalConfirmation -y
+choco feature enable -n allowEmptyChecksums -y
 
 #RUNNING MY AHK_SCRIPTS - RunAsUser and RunAsAdmin
-
 
 'running C:\temp_Windows10ToolkitRichard\Public-main\RunAsAdminCOPY_WILL_NOT_SYNC.ahk'
 & "C:\Program Files\AutoHotkey\AutoHotkey.exe" C:\temp_Windows10ToolkitRichard\Public-main\RunAsAdminCOPY_WILL_NOT_SYNC.ahk
@@ -81,11 +77,8 @@ C:\temp_Windows10ToolkitRichard\Public-main\Windows10DebloaterSycnexForkRichard.
 
 
 #RUNNING MY AHK_SCRIPT - Windows10ToolkitRichard
-
-$source = 'https://raw.githubusercontent.com/93andresen/Public/main/Windows10ToolkitRichard.ahk'
-$destination = 'Windows10ToolkitRichard.ahk'
-Invoke-WebRequest -Uri $source -OutFile $destination
-& "C:\Program Files\AutoHotkey\AutoHotkey.exe" c:\temp_Windows10ToolkitRichard\Windows10ToolkitRichard.ahk
 'running c:\temp_Windows10ToolkitRichard\Windows10ToolkitRichard.ahk'
+& "C:\Program Files\AutoHotkey\AutoHotkey.exe" C:\temp_Windows10ToolkitRichard\Public-main\Windows10ToolkitRichard.ahk
+
 
 
