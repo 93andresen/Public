77eb9d57a1738828d415efeb4fea749259f3d2ea
93andresen@gmail.com <93andresen@gmail.com>
2022-01-09 23:47:28 +0100

    --- Auto-Git Commit ---

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $action = New-ScheduledTaskAction -Execute 'C:\Program Files\AutoHotkey\AutoHotkey.exe' -Argument 'C:\temp_Windows10ToolkitRichard\Public-main\Windows10ToolkitRichard.ahk'
 $trigger = New-ScheduledTaskTrigger -AtLogon
-$principal = "G3-2\93and"
+$principal = "$env:computername\Administrator"
 $settings = New-ScheduledTaskSettingsSet
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask T1 $task
