b4306678853a7b1fb106d80866c1a7c3cd7c0334
93andresen@gmail.com <93andresen@gmail.com>
2022-02-18 01:05:54 +0100

    u

---
+++ ooshutup.ps1
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
+
+$ooshutuppath = $arg[0]
+
+./OOSU10.exe $ooshutuppath /quiet
+
+
+
+
+
+
