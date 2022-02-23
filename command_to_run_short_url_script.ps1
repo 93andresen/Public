f825386c2be3a1ed2e8d9badad0c7c4086f53adc
93andresen@gmail.com <93andresen@gmail.com>
2022-02-23 12:57:10 +0100

    ui

--- command_to_run_short_url_script.ps1
+++ command_to_run_short_url_script.ps1
@@ -11,7 +11,7 @@ iwr git.io/J13Mt -UseBasicParsing|iex
 
 
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;mkdir C:\!\Code\GitHub\93andresen_Scripts\Public -ErrorAction SilentlyContinue;Invoke-WebRequest -Uri https://git.io/J13Mt -OutFile C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 -ErrorAction SilentlyContinue;C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 nogui -ErrorAction SilentlyContinue
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;mkdir C:\!\Code\GitHub\93andresen_Scripts\Public -ErrorAction SilentlyContinue;Invoke-WebRequest -Uri https://git.io/J13Mt -OutFile C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 -ErrorAction SilentlyContinue;C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 update debloat -ErrorAction SilentlyContinue
 
 
 
