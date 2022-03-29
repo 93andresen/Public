db5e7902daf50d64704094d8fd121131e3a0a5c2
93andresen@gmail.com <93andresen@gmail.com>
2022-03-30 01:20:51 +0200

    command

---
+++ command_to_run_short_url_script.ps1
@@ -0,0 +1,58 @@
+
+
+
+Windows10Perfect - ooshutup3 for Maximum Privacy
+
+iwr https://git.io/J13Mt -UseBasicParsing|iex
+
+
+iwr git.io/J13Mt -UseBasicParsing|iex
+
+
+====================================================================
+Personal ooshutop1: Essesinal Tweaks (Probably Best For Most People)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;mkdir C:\!\Code\GitHub\93andresen_Scripts\Public -ErrorAction SilentlyContinue;Invoke-WebRequest -Uri https://git.io/J13Mt -OutFile C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 -ErrorAction SilentlyContinue;C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 update debloat darktsk reboot ooshutup1 dns4c6a personal
+
+Personal ooshutop3: Essesinal Tweaks + Disable App Access (For The Paranoid)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;mkdir C:\!\Code\GitHub\93andresen_Scripts\Public -ErrorAction SilentlyContinue;Invoke-WebRequest -Uri https://git.io/J13Mt -OutFile C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 -ErrorAction SilentlyContinue;C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 update debloat darktsk reboot ooshutup3 dns4c6a personal
+
+Personal ooshutop4: Undo All Changes (Revert To Factory Settings)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;mkdir C:\!\Code\GitHub\93andresen_Scripts\Public -ErrorAction SilentlyContinue;Invoke-WebRequest -Uri https://git.io/J13Mt -OutFile C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 -ErrorAction SilentlyContinue;C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 update debloat darktsk reboot ooshutup4 dns4c6a personal
+====================================================================
+
+
+
+ooshutop1: (Essesinal Tweaks (Probably Best For Most People)) This has none other flags then ooshutup1
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;mkdir C:\!\Code\GitHub\93andresen_Scripts\Public -ErrorAction SilentlyContinue;Invoke-WebRequest -Uri https://git.io/J13Mt -OutFile C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 -ErrorAction SilentlyContinue;C:\!\Code\GitHub\93andresen_Scripts\Public\Windows10ToolkitRichard.ps1 ooshutup1
+
+
+update debloat apps netflix darktsk reboot ooshutup01234 dns4*6*
+
+ooshutup1, Essesinal Tweaks (Probably Best For Most People)
+ooshutup2, Essesinal Tweaks + Disable Microphone And Webcam
+ooshutup3, Essesinal Tweaks + Disable App Access (For The Paranoid)
+ooshutup4, Undo All Changes (Revert To Factory Settings)
+ooshutup0, Don't Do Anything - Will Keep These Settings As is
+
+
+dns4a6a ipv4=auto          ipv6=auto
+dns4c6c ipv4=cloudflare    ipv6=cloudflare
+dns4a6c ipv4=auto          ipv6=cloudflare
+dns4c6a ipv4=cloudflare    ipv6=auto
+
+#iwr https://git.io/J13Mt|iex
+#iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/J13Mt'))
+
+iwr git.io/J13iA|iex
+#iwr https://git.io/J13iA|iex
+
+#iwr git.io/J1kbW|iex
+#iwr https://git.io/J1kbW|iex
+
+#iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/J1kbW'))
+
+
+#iwr git.io/JXaaD|iex
+#iwr https://git.io/JXaaD|iex
+
+#iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JXaaD'))
